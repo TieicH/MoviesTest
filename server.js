087e6a111543c2c3d120faae8957b39f2fc81cdc
TH <e.vilela.m@gmail.com>
2018-12-05 07:11:35 -0500

    [UPDATE] changing server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const next = require("next");
 const routes = require("./routes");
 const app = next({ dev: process.env.NODE_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const { createServer } = require("http");
 app.prepare().then(() => {
